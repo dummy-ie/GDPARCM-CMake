d68f914a604e9eb8221eb1c080304f9124252206
dummy-ie <115205154+dummy-ie@users.noreply.github.com>
2025-01-08 12:17:15 +0800

    Set C++20 standard in CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+#include <concepts>
+
 #include <SFML/Graphics.hpp>
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "GDPARCM CMake Test");
     window.setFramerateLimit(144);
+
+    std::cout << "C++ Version: " << __cplusplus;
 
     while (window.isOpen())
     {
