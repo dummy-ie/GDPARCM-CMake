15aea1d1b76b4772ad157d29fce6a9d20732feba
dummy-ie <115205154+dummy-ie@users.noreply.github.com>
2025-01-30 21:45:04 +0800

    Update IETThread

--- src/Controller/IETThread.cpp
+++ src/Controller/IETThread.cpp
@@ -1,19 +1,19 @@
 #include "IETThread.h"
 
+using namespace gd;
+
 IETThread::IETThread()
-{
-}
+= default;
 
 IETThread::~IETThread()
-{
-}
+= default;
 
 void IETThread::start()
 {
 	std::thread(&IETThread::run, this).detach(); //detach thread for independent execution. without this, join() function must be called.
 }
 
-void IETThread::sleep(int ms)
+void IETThread::sleep(const int ms)
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
--- src/Controller/IETThread.h
+++ src/Controller/IETThread.h
@@ -6,16 +6,24 @@
 /// Representation of a Java/C# like thread class. The behavior must be inherited by a class that needs to be threaded.
 /// By: NeilDG
 /// </summary>
-class IETThread
+namespace gd
 {
-public:
-	IETThread();
-	~IETThread();
+	class IETThread
+	{
+	public:
+		IETThread();
+		virtual ~IETThread();
 
-	void start(); //schedules thread execution
-	static void sleep(int ms);
-	
-protected:
-	virtual void run() = 0;
-};
+		IETThread(const IETThread&) = default;
+		IETThread(IETThread&&) = default;
+		IETThread& operator=(const IETThread&) = default;
+		IETThread& operator=(IETThread&&) = default;
+
+		void start(); //schedules thread execution
+		static void sleep(int ms);
+
+	protected:
+		virtual void run() = 0;
+	};
+}
 
